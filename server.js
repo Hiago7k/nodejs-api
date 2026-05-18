22f0f4b31c3d540c8c2bfd2a3772db33cf57e175
Hiago 7k <hiago7k@gmail.com>
2026-05-18 18:54:09 -0300

    utilizando express

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000;
 
-const hostname = '127.0.0.1'
-const port = 3000
 
-const server = http.createServer((req, res) =>{
-    res.statusCode = 200
-    res.setHeader('Contet-Type', 'text/plain')
-    res.end('Hello World\n')
+app.get('/', (req, res) =>{
+    res.send('Criando um servidor em expresscs');
 })
 
-server.listen(port, hostname, () =>{
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Server on in port ${port}`);
 })
