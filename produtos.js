f712199e02005b9e44dc3ecb22541699ec47caac
Hiago 7k <hiago7k@gmail.com>
2026-05-20 21:16:32 -0300

    criando um mini crud em memoria

--- produtos.js
+++ produtos.js
@@ -2,26 +2,33 @@ import express from 'express';
 
 const router = express.Router();
 
-
-router.get('/', (req, res) => {
-    res.json([
-       {
+const produtos = [
+    {
         nome: "Hiago",
         telefone: "9999-1111",
         data: "2015-04-12T12:53:46.2042",
-       },
-       {
-        nome: "Ricardo",
-        telefone: "2329-1246",
-        data: "2016-02-12T12:53:46.2041",
-        
-       },
-       {
-        nome: "Jose",
-        telefone: "2342-2575",
-        data: "2026-11-12T12:33:10.2050",
-       },
-    ]);
+    },
+    {  nome: "Ricardo",
+            telefone: "2329-1246",
+            data: "2016-02-12T12:53:46.2041",},
+    {
+         nome: "Jose",
+            telefone: "2342-2575",
+            data: "2026-11-12T12:33:10.2050",
+    },
+];
+
+
+router.get('/', (req, res) => {
+       res.json(produtos);
 });
+
+
+router.post('/', (req, res) => {
+    produtos.push(req.body);
+
+
+    res.status(201).json(req.body);
+})
 
 export default router;
